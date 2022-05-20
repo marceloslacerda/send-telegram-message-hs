de1ef18d5215e5ce064f691360b6d916f21a1096
Marcelo Lacerda <marceloslacerda@users.noreply.github.com>
2022-05-20 01:09:06 +0000

    Add todo

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,16 @@ module Lib
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+-- todo:
+-- get message from args
+-- try to get database parameters from env
+-- if could not get, guess it
+-- if cannot guess just try  localhost
+-- format to payload
+-- try to connect to database
+--   see if similar message was sent before
+--   if sent change the message to reflect that
+-- if the database communication failed for any reason, use the previous message
+-- send message to  telegram
+-- try to save the message sent to the database
